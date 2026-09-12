d26ad27d6e5e456cf2892daef880e2c04d267b99
FedericoBrid <federicobrid@gmail.com>
2026-09-12 11:49:08 -0300

    starting springboot project + configuration bbdd

---
+++ src/main/java/com/donar/api/DonarApiApplication.java
@@ -0,0 +1,13 @@
+package com.donar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donar/api/DonarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donar.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
