2f9de09e46230e596e2a4760898d1f604dae0918
FedericoBrid <federicobrid@gmail.com>
2026-09-15 12:21:24 -0300

    add bloodTypeService

---
+++ src/main/java/com/donar/api/bloodtype/service/BloodTypeService.java
@@ -0,0 +1,24 @@
+package com.donar.api.bloodtype.service;
+
+import com.donar.api.bloodtype.entity.BloodType;
+import com.donar.api.bloodtype.repository.IBloodTypeRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BloodTypeService {
+
+    private final IBloodTypeRepository bloodTypeRepository;
+
+    public List<BloodType> findAll() {
+        return bloodTypeRepository.findAll();
+    }
+
+    public BloodType findById(Long id) {
+        return bloodTypeRepository.findById(id).orElseThrow(() -> new RuntimeException("Blood type not found with id: " + id));
+    }
+}
